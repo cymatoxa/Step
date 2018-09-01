4e467e530c4b42f7515021fd0a39e6ad97cd5bd5
Alioshkin <cymatoxa328@gmail.com>
2018-09-01 20:28:16 +0300

    Create Writer class

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,9 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hi");
-
+		Writer writer = new Writer();
+		writer.setName("Bob");
+		System.out.println(writer.toString());
 	}
 
 }
---
+++ src/Writer.java
@@ -0,0 +1,17 @@
+
+public class Writer {
+private String name;
+
+public String getName() {
+	return name;
+}
+
+public void setName(String name) {
+	this.name = name;
+}
+
+@Override
+public String toString() {
+	return "Writer [name=" + name + "]";
+}
+}
